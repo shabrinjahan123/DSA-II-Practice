3b831000b432d8d350aaaa027dbbf4993a9094c3
shabrinjahan123 <shabrinjahan230@gmail.com>
2026-04-01 00:46:26 +0600

    Add Find_the_median.cpp

---
+++ Find_the_median.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findMedian(vector<int>arr){
+    sort(arr.begin(),arr.end());
+    int n=arr.size();
+    return arr[n/2];
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<findMedian(arr)<<endl;
+    return 0;
+}
